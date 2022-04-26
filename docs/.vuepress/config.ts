b876b299bfb8eccb777dbf9592cf08d259fe982e
riri <daydreamerriri@outlook.com>
2022-04-26 11:50:06 +0800

    commit

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -64,7 +64,7 @@ export default defineUserConfig<DefaultThemeOptions, ViteBundlerOptions>({
     ],
     title: 'Material You',
     description: 'A material you components library',
-    base: '/MaterialYou-UI/',
+    base: '/',
 
     // 注册组件
     clientAppEnhanceFiles: path.resolve(__dirname, './clientAppEnhance.js'),
